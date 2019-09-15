9afd189517f0754e4fc97da1e83004a22120d17b
handy <handynugraha29@gmail.com>
2019-09-15 20:12:47 +0700

    Sampai Video 10. Menambahkan component yaitu Navbar. Hapus file-file yang gak dibutuhkan.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			// returnnya, harus one parent element kalau di react jsx.
+      <div className="App">
+        <Navbar />
+      </div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/layout/Navbar.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Navbar extends React.Component {
+	static defaultProps = {
+		title: 'Github Finder',
+		icon: 'fab fa-github'
+	};
+
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		icon: PropTypes.string.isRequired
+	};
+
+	render() {
+		return (
+			<nav className="bg-blue navbar">
+				<h1>
+					<i className={this.props.icon} /> {this.props.title}
+				</h1>
+			</nav>
+		);
+	}
+}
+
+export default Navbar;
